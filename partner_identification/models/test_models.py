7e5e2978150bd9d93167b6cffaa0b42c6fcb865a
Daniel Blanco <daniel@blancomartin.cl>
2018-05-27 16:22:11 -0400

    [LINT] Revision phase1

--- partner_identification/models/test_models.py
+++ partner_identification/models/test_models.py
@@ -1,6 +1,6 @@
 # Copyright 2017 LasLabs Inc.
 # Copyright 2017 Camptocamp
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models, tools
 import os
